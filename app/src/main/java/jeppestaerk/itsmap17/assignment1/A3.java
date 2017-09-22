db3929cc888e018a616ab05da1579b6223589192
Jeppe Stærk <jeppe@staerk.io>
2017-09-22 22:46:07 +0200

    Design init

---
+++ app/src/main/java/jeppestaerk/itsmap17/assignment1/A3.java
@@ -0,0 +1,13 @@
+package jeppestaerk.itsmap17.assignment1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class A3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a3);
+    }
+}
